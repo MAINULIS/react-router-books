5a28c3a7c4f786f3bd615b7435246ecdeebe572d
Mainul Islam <mainulislam6168@gmail.com>
2023-08-18 17:11:57 +0600

    react-router & tailwindcss rendered and components file created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default App;
