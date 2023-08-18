6475a42de6b5099e975b465da28eb3177f0920c4
Mainul Islam <mainulislam6168@gmail.com>
2023-08-18 22:53:51 +0600

    home page and animation added & custom class added

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Books = () => {
+    const booksData = useLoaderData();
     return (
         <div>
             <h3>This is books corner</h3>
