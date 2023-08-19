c6e393059b2d37aa433c2a5d6a91b88b22929362
Mainul Islam <mainulislam6168@gmail.com>
2023-08-19 17:45:03 +0600

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
-      {/* Header section */}
       <Header></Header>
-    <Outlet></Outlet>
-    {/* Footer section */}
+     <div className='min-h-[calc(100vh-136px)]'>
+     <Outlet></Outlet>
+     </div>
+    <Footer></Footer>
     </div>
   );
 };
